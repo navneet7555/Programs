f98766b9476921905fd2acd7a3f7ace2cb16def2
navneet7555 <navneetpandey2602@gmail.com>
2025-09-30 16:39:48 +0530

    Programs

---
+++ practice/pp.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int is_student=1;
+    if(is_student==1){
+        printf("You are a student");
+    }else{
+        printf("You are not a student");
+    }
+    return 0;
+}
